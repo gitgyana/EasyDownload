7da152235717c92aa273008c88c06414690ebb97
Gyana Priyadarshi <gyana.career@gmail.com>
2025-09-30 18:06:40 +0530

    Make credential files

---
+++ credentials/credentials_alldebrid.py
@@ -0,0 +1,2 @@
+alldebrid_api_key_list = [alldebrid_api_key1, alldebrid_api_key2, ...]
+alldebrid_test_link = r"https://example.com/path/to/file.one"
---
+++ credentials/credentials_streamtape.py
@@ -0,0 +1,2 @@
+streamtape_api_username_list = [streamtape_api_username1, streamtape_api_username2, ...]    # streamtape login
+streamtape_api_password_list = [streamtape_api_password1, streamtape_api_password2, ...]    # streampate API key
