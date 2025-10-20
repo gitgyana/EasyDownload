d1348773cd3c5e5a87900ed9e5efd1c06524b927
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-21 00:00:42 +0530

    Insert normalized file directory

--- database_manager.py
+++ database_manager.py
@@ -1,3 +1,4 @@
+import os 
 import time
 import sqlite3
 
@@ -33,3 +34,38 @@ def was_file_downloaded(file_dir, sqlite_db="downloads_db.db", table_name="downl
         time.sleep(10)
         return False
 
+
+def insert_file_path(file_dir, sqlite_db="downloads_db.db", table_name="downloads"):
+    try:
+        if not sqlite_db:
+            log("error", "Invalid or missing downloads database file.")
+            return False
+        
+        file_dir = os.path.normpath(file_dir) 
+        
+        base_dir = os.getcwd() 
+        file_dir = os.path.relpath(file_dir, base_dir) 
+
+        conn = sqlite3.connect(sqlite_db)
+        cursor = conn.cursor()
+        
+        cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (id INTEGER PRIMARY KEY, file_path TEXT UNIQUE)")
+        
+        cursor.execute(f"INSERT OR IGNORE INTO {table_name} (file_path) VALUES (?)", (file_dir,))
+        
+        conn.commit()
+        conn.close()
+        
+        log("info", f"File path inserted: {file_dir}")
+        return True
+    
+    except sqlite3.Error as e:
+        log("error", f"SQLite error: {e}")
+        return False
+    except Exception as func_err:
+        log("error", f"ensure_db_and_insert_file_path function error: {func_err}")
+        return False
+    except KeyboardInterrupt:
+        log("critical", "KeyboardInterrupt: Exited from function [ensure_db_and_insert_file_path]")
+        time.sleep(10)
+        return False
