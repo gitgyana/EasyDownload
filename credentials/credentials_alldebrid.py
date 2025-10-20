7bd56777d8984d3d7dd250c7c50e4c3b4bc20a64
Gyana Priyadarshi <gyana.career@gmail.com>
2025-10-20 19:52:45 +0530

    Stop tracking credentials/credentials_alldebrid.py and credentials/credentials_streamtape.py

--- credentials/credentials_alldebrid.py
+++
@@ -1,2 +0,0 @@
-alldebrid_api_key_list = [alldebrid_api_key1, alldebrid_api_key2, ...]
-alldebrid_test_link = r"https://example.com/path/to/file.one"
--- credentials/credentials_streamtape.py
+++
@@ -1,2 +0,0 @@
-streamtape_api_username_list = [streamtape_api_username1, streamtape_api_username2, ...]    # streamtape login
-streamtape_api_password_list = [streamtape_api_password1, streamtape_api_password2, ...]    # streampate API key
